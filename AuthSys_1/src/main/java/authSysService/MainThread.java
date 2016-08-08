e4fe6f6c3994923b3ecc9231bc16e6059293c0e9
A T <WEO@host-88-64.soho.net.ua>
2016-08-08 16:31:13 +0300

    edition 1

--- AuthSys_1/src/main/java/authSysService/MainThread.java
+++ AuthSys_1/src/main/java/authSysService/MainThread.java
@@ -21,37 +21,27 @@ public class MainThread {
     public Properties userProperty = new Properties();
 
 
-    public MainThread () {
+    public MainThread() {
 
         InputStream adminResourceAsStream = MainThread.class.getResourceAsStream(adminPath);
         InputStream userResourceAsStream = MainThread.class.getResourceAsStream(userPath);
 
         try {
             adminProperty.load(adminResourceAsStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
             userProperty.load(userResourceAsStream);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
-    public static UserRole authCheck(String username, String password) {
+    public UserRole authCheck(String username, String password) {
 
-        MainThread mainThread = new MainThread();
-
-        System.out.println("username= "+ username);
-        System.out.println("username chk= " +mainThread.adminProperty.getProperty(username));
-        System.out.println("password= "+password);
-        System.out.println("password chk= "+mainThread.adminProperty.getProperty(password));
-
-        if ((username.equals(mainThread.adminProperty.getProperty(username))) &&
-                (password.equals(mainThread.adminProperty.getProperty(password)))) {
+        if ((username.equals(this.adminProperty.getProperty(username))) &&
+                (password.equals(this.adminProperty.getProperty(password)))) {
             userRole = ADMIN;
-        } else if ((username.equals(mainThread.userProperty.getProperty(username))) &&
-                (password.equals(mainThread.userProperty.getProperty(password)))) {
+        } else if ((username.equals(this.userProperty.getProperty(username))) &&
+                (password.equals(this.userProperty.getProperty(password)))) {
             userRole = USER;
         }
         return userRole;
--- AuthSys_1/src/main/java/userBase/User.java
+++ AuthSys_1/src/main/java/userBase/User.java
@@ -19,6 +19,30 @@ public class User {
 
     }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
     @Override
     public String toString() {
         return "User{" +
